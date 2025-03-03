e537a93aef2ab0c180b10d16d5a09731c4981ea9
Danilozxz <progdanilo@gmail.com>
2025-03-03 12:30:55 -0300

    chore: estrutura inicial do projeto

---
+++ src/main/java/com/bootcamp/avanade/api_rede/Application.java
@@ -0,0 +1,13 @@
+package com.bootcamp.avanade.api_rede;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/bootcamp/avanade/api_rede/ApiRedeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.avanade.api_rede;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRedeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
