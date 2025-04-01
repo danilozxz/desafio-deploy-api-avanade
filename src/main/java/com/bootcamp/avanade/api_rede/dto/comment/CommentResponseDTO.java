1c0af1a889a7186b3a3fd56cbdb071c79587e8a7
Danilozxz <progdanilo@gmail.com>
2025-03-31 21:55:15 -0300

    fix: correção para exibir id do comment no response

--- src/main/java/com/bootcamp/avanade/api_rede/dto/comment/CommentResponseDTO.java
+++ src/main/java/com/bootcamp/avanade/api_rede/dto/comment/CommentResponseDTO.java
@@ -4,9 +4,10 @@ import java.time.LocalDateTime;
 
 import com.bootcamp.avanade.api_rede.model.Comment;
 
-public record CommentResponseDTO(Long userId, Long postId, String content, LocalDateTime createdAt) {
+public record CommentResponseDTO(Long id, Long userId, Long postId, String content, LocalDateTime createdAt) {
     public static CommentResponseDTO from(Comment comment) {
         return new CommentResponseDTO(
+            comment.getId(),
             comment.getUser().getId(),
             comment.getPost().getId(),
             comment.getContent(),
