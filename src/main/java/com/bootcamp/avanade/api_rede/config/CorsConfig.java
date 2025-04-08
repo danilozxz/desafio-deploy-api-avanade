8bfba7130289986d00a29f32931a5c651e921795
Danilozxz <progdanilo@gmail.com>
2025-04-07 22:11:19 -0300

    fix: CORS swagger

--- src/main/java/com/bootcamp/avanade/api_rede/config/CorsConfig.java
+++ src/main/java/com/bootcamp/avanade/api_rede/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200", "https://decola-feed-ui.vercel.app")
+                        .allowedOrigins("http://localhost:4200", "https://decola-feed-ui.vercel.app", "https://desafio-deploy-api-avanade-production.up.railway.app/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
